5e8a56272ffb843726672c7000262d582b2449f4
Himanshu Shekhar <Himanshu.Shekhar@jp.sony.com>
2018-03-01 16:03:03 +0900

    Hunterize PCRE

--- Foundation/cmake/PocoFoundationConfig.cmake
+++ Foundation/cmake/PocoFoundationConfig.cmake
@@ -2,6 +2,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/PocoFoundationTargets.cmake")
 
 if (@POCO_UNBUNDLED@)
     if (@HUNTER_ENABLED@)
+        find_package(libpcre CONFIG REQUIRED)
         find_package(ZLIB CONFIG REQUIRED)
     endif()
 endif()
